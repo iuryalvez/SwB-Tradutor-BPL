4f9f2e3e1fb4b7770cbb9b6be571d1eb5debc3ed
Iury Bo <iuryalves774@gmail.com>
2023-08-10 22:51:51 -0300

    mudanças drásticas

--- include/funcoes.h
+++ include/funcoes.h
@@ -5,125 +5,55 @@
 #include "registradores.h"
 #include <stdbool.h>
 
-// armazena informações padrões de uma variável local de pilha
-struct variavel_local {
-    int indice;
-    int pos_pilha;
-    int size;
-};
+typedef struct Var{
+    int ind; // índice da variável
+    int pos; // posição na pilha da variável
+} Var;
 
-typedef struct variavel_local vl;
+typedef struct vet {
+    int ind; // índice da variável
+    int pos; // posição na pilha da variável
+    int size; // size do vet
+} Vet;
+
+typedef struct reg {
+    int ind; // índice da variável
+    int pos; // posição na pilha da variável
+} Reg;
+
+typedef struct VL {
+    Var var[4]; // vetor que guarda as vars
+    Vet vet[4]; // vetor que guarda os arrays
+    Reg reg[4]; // vetor que guarda os registradores
+    int var_qtd; // quantidade de var
+    int vet_qtd; // quantidade de vet
+    int reg_qtd; // quantidade de reg
+} VL;
+
+typedef struct param {
+    int qtd;
+    char tipo[3];
+    int pos[3];
+    int subq;
+} Param;
+
+void inicializar_param(Param *param);
+
+void salvar_parametros(Param *param, int *rsp);
+
+void recuperar_parametros(Param *param, int *rsp);
 
 /**
- * @brief Inicializa ou reinicializa todas as variaveis de controle
+ * @brief inicializa os valores da struct de variáveis locais com 0
  * 
- * @param indice_funcao variavel de controle
- * @param nro_param variavel de controle
- * @param tipo_param variavel de controle
- * @param tampilha variavel de controle
- * @param vlp variavel de controle
- * @param ind_reg variavel de controle
- * @param cont_reg variavel de controle
- * @param cont_vlp variavel de controle
+ * @param vl ponteiro para a struct de variáveis locais
  */
-void reinicializa_parametros(int *indice_funcao,int *nro_param, char * tipo_param, int *tampilha, vl * vlp, int * ind_reg, int * cont_reg, int * cont_vlp);
+void inicializar_vl(VL *vl);
 
-/**
- * @brief Printa todas as variaveis de controle
- * 
- * @param indice_funcao variavel de controle
- * @param nro_param variavel de controle
- * @param tipo_param variavel de controle
- * @param tampilha variavel de controle
- * @param vlp variavel de controle
- * @param ind_reg variavel de controle
- * @param cont_reg variavel de controle
- * @param cont_vlp variavel de controle
- */
-void printacontrole(int indice_funcao, int nro_param, char * tipo_param, int tampilha, vl * vlp, int * ind_reg, int cont_reg, int cont_vlp);
+void armazenar_vl(VL *vl);
 
-/**
- * @brief salva os parâmetros de uma função acordo com sua quantidade
- * 
- * @param nro_param número de parâmetros da função
- * @param tampilha tamanho da pilha da função
- * @param r registradores 
- */
-void salvar_parametros(int nro_param, int *tampilha, Registrador r[MAX_REG]);
+void recuperar_vl(VL vl);
 
-/**
- * @brief recupera os parâmetros de uma função acordo com sua quantidade
- * 
- * @param nro_param número de parâmetros da função
- * @param tampilha tamanho da pilha da função
- * @param r registradores
- */
-void recuperar_parametros(int nro_param, int *tampilha, Registrador r[MAX_REG]);
-
-/**
- * @brief iniciar valores padrão (-1) no vetor de variáveis locais
- * 
- * @param vlp variáveis locais de pilha
- */
-void iniciar_vlp(vl vlp[8]);
-
-/**
- * @brief armazenamento de variáveis nos registradores 
- * 
- * @param ind_reg índice dos registradores
- * @param qtd_reg quantidade de variáveis de registradores
- * @param r registradores
- */
-void armazenar_vr(int ind_reg[4], int qtd_reg, Registrador r[MAX_REG]);
-
-/**
- * @brief armazenamento das variáveis locais da pilha
- * 
- * @param vlp variáveis locais de pilha
- * @param qtd_vlp quantidade de variáveis locais de pilha
- * @param tampilha tamanho da pilha
- */
-void armazenar_vlp(vl vlp[8], int qtd_vlp, int tampilha);
-
-/**
- * @brief alinha o tamanho da pilha para o múltiplo do alinhamento desejado
- *  
- * @param tampilha tamanho da pilha da função
- * @param alinhamento alinhamento desejado
- */
-void alinhar_tampilha(int * tampilha, int alinhamento);
-
-/**
- * @brief Recupera os registradores salvos
- * 
- * @param r Vetor de registradores
- * @param tampilha Posição final da pilha
- * @param salvos Vetor que possui o index dos registradores salvos
- */
-void recuperar_reg(Registrador *r, int * tampilha, int * salvos);
-
-/**
- * @brief Salva registradores
- * 
- * @param r Vetor de registradores
- * @param tampilha Posição final da pilha
- * @param salvos Vetor que possui o index dos registradores salvos
- */
-void salvar_reg(Registrador *r, int * tampilha, int * salvos);
-
-/**
- * @brief Multiplo de 16
- * 
- * @param x Numero analisado
- * @return int x é analisado, se for multiplo de 8, retorna o multiplo de 16 mais proximo acima,se for multiplo de 16 retorna o proprio x
- */
-int multiplode16(int x);
-
-/**
- * @brief inicializa o vetor de salvos
- * 
- * @param salvos Vetor que possui o index dos registradores salvos
- */
-void inicializasalvos(int * salvos);
+void alinhar_rsp(int *rsp, int alinhamento);
 
 #endif
--- tradutor.c
+++ tradutor.c
@@ -1,85 +1,30 @@
 #include "./include/tradutor.h"
 
-/*  Última modificação 06/08 - 15h13
-
-    Modificações IURY (06/08 - 2h51): 
-    
-    -> formatação: organizei algumas formatações. obs: utilizar o tab do teclado e não o \t porque o \t é muito grande;
-    
-    -> organização: passei o código principal que estava na "call_funcao()" para a main porque faz mais sentido, percebi
-                    que o código não precisa mais ter a ideia de recursão também;
-
-    -> bloco_def: implementei algumas regras de variáveis locais (bloco_def), o conceito de tamanho da
-       pilha (junto com a ideia de múltiplo), criei novas variáveis (vlp, ind_reg); 
-    
-    -> problemas: 
-        - 1. criei as funções de salvar e recuperar os registradores de parâmetros ao ler um call, mas tem
-          algo errado na implementação, verificar depois e corrigir; 
-        - 2. ao compilar o código surge alguns warnings em relação ao uso da struct registrador, verificar 
-          depois e corrigir.
-
-    Modificações IURY (06/08 - 14h24):
-    
-    -> arquivo shell: acrescentei o clear antes e exclui o arquivo test depois de executá-lo para que caso um programa
-       que não deve compilar compile. 
-
-    -> refactor: refatorei os códigos de armazenar as variáveis e os registradores, mas não sei até que ponto está dando certo;
-    
-    -> fix problema 1: implementei as funções de salvar e recuperar parâmetros antes da chamada de função;
-    
-    -> fix problema 2: o problema não estava deixando o código compilar, faltava as declarações ifndef nos arquivos .h
-       tentei criar uma variável global (entre os arquivos) para representar os registradores mas não consegui, tive
-       que continuar com a implementação da variável dentro do bloco da main, com o ifndef agora está funcionando.
-
-    -> o que precisa ser feito: reavaliar a função de alinhar pilha para ter certeza que vai funcionar em todos os casos 
-*/
-
 int main() {
-
-    char nomefuncao[128];
-
-
-    int salvos[5]; // de 0 a 3 salva o index dos registradores salvos e na posição 4 salva quantos registradores foram salvos.
-
-
     // cabeçalho de um arquivo assembly
     printf(".section rodata\n\n");
     printf(".data\n\n");
     printf(".text\n\n");
     
-    Registrador r[MAX_REG];
+    int rsp = 0;     // tamanho da pilha da função
+    // int padding = 0; // armazenar o padding da pilha
 
     char linha[TAM_LINHA]; // armazena a linha lida do arquivo 
 
     // variáveis de declaração de função
-    int indice_funcao = 0;     // índice da função
-    int nro_param = 0;         // número de parâmetros da função
-    char tipo_param[3];    // tipo de parâmetro
+    int indice_funcao = 0; // índice da função
+    char nomefuncao[5]; // f9999 (5 letras)
+    
+    Param param; // variável de parâmetro (declaração da função)
+    inicializar_param(&param);
 
-    // variáveis de controle e contagem da pilha
-    int tampilha = 0; // tamanho da pilha da função
-
-    // variáveis de controle da pilha
-    vl vlp[8]; // variáveis locais de pilha
-    int ind_reg[4];   // índice dos registradores
-    int cont_reg = 0; // contador de registradores
-    int cont_vlp = 0; // contador de variaveis locais de pilha
-
-    reinicializa_parametros(&indice_funcao, &nro_param, tipo_param, &tampilha, vlp, ind_reg, &cont_reg, &cont_vlp); // inicializando parametros
+    VL vl; // variáveis locais (def e enddef)
+    inicializar_vl(&vl);
 
     // flags de controle
     bool bloco_declaracao = true; // flag do bloco de declaração da função
     bool bloco_def = false;       // flag do bloco de definição de variáveis locais da função  
     bool bloco_funcao = false;    // flag do bloco de execução da função
-
-    /*
-        Testando as funções
-    
-        printacontrole(indice_funcao, nro_param, tipo_param, tampilha, vlp, ind_reg, cont_reg, cont_vlp);
-        reinicializa_parametros(&indice_funcao, &nro_param, tipo_param, &tampilha, vlp, ind_reg, &cont_reg, &cont_vlp);
-        printf("\n\nReiniciliazado\n\n");
-        printacontrole(indice_funcao, nro_param, tipo_param, tampilha, vlp, ind_reg, cont_reg, cont_vlp);
-    */
 
     while(ler_linha(linha) != false) { // continua o loop linha a linha enquanto não chega numa linha nula ou no EOF 
 
@@ -89,9 +34,15 @@ int main() {
             
             if (strncmp(linha, "function", 8) == 0){ // verifica se a linha inicia com a declaração de uma função
 
-                nro_param = sscanf(linha, "function f%d p%c1 p%c2 p%c3", &indice_funcao, &tipo_param[0], &tipo_param[1], &tipo_param[2]);
+                rsp = 0;
+                // padding = 0;
+                inicializar_param(&param);
 
-                if (nro_param == 0 || nro_param > 4) {
+                param.qtd = sscanf(linha, "function f%d p%c1 p%c2 p%c3", &indice_funcao, &param.tipo[0], &param.tipo[1], &param.tipo[2]);
+                
+                --param.qtd;
+
+                if (param.qtd == -1 || param.qtd > 3) {
                     printf("Tem algo de errado na declaracao da funcao.\n");
                     return 0;
                 }
@@ -109,51 +60,48 @@ int main() {
                 bloco_declaracao = false; // finaliza a declaração
                 bloco_def = true; // permite o início da definição de variáveis
 
-                iniciar_registradores(r);
+                // iniciar os valores das variáveis para a próxima função
+                inicializar_vl(&vl);
                 
                 continue;
             }
-
         }
 
         if (bloco_def) {
-
             if (strncmp(linha, "enddef", 6) == 0) { // verifica se a linha é o fim da definição de variáveis
                 bloco_def = false;   // finaliza a definição de variáveis
                 bloco_funcao = true; // permite o início do corpo da função
                 
-                // se houver variáveis locais de registradores, armazenamos
-                if (cont_reg) armazenar_vr(ind_reg, cont_reg, r); 
+                // padding = rsp; 
+                alinhar_rsp(&rsp, 16); // se houver padding, rsp será atualizado
+                // padding = rsp - padding; 
                 
-                // se houver variáveis locais de pilha, armazenamos
-                if (cont_vlp) armazenar_vlp(vlp, cont_vlp, tampilha);
+                printf("    subq    $%d, %%rsp\n", rsp);
 
+                armazenar_vl(&vl);
+                
                 continue;
             }
 
-            // código da definição de variáveis aqui
-            if (sscanf(linha, "reg vr%d", &ind_reg[cont_reg]) == 1) {
-                
-                cont_reg++;
-                
-            }
-            if (sscanf(linha, "var vi%d", &vlp[cont_vlp].indice) == 1) {
-                
-                vlp[cont_vlp].size = 1;
-                alinhar_tampilha(&tampilha, 4); // alinhando a pilha para um endereço mútiplo de 4 (int)
-                tampilha += 4; // atualizando o valor da pilha
-                vlp[cont_vlp].pos_pilha = tampilha; // a posição da pilha na variável é o valor do tamanho da pilha
-                cont_vlp++; // atualizando o contador de variáveis locais
-                
+            // lendo as variáveis locais e registrando suas posições na pilha
+            if (sscanf(linha, "reg vr%d", &vl.reg[vl.reg_qtd].ind) == 1 && indice_funcao != 1) {
+                alinhar_rsp(&rsp, 8);
+                rsp += 8;
+                vl.reg[vl.reg_qtd].pos = rsp;
+                vl.reg_qtd++;
                 continue;
             }
-            if (sscanf(linha, "vet va%d size ci%d", &vlp[cont_vlp].indice, &vlp[cont_vlp].size) == 2) {
-                
-                alinhar_tampilha(&tampilha, 8); // alinhando a pilha para um endereço múltiplo de 8 (ponteiro)
-                tampilha += 8*vlp[cont_vlp].size; // atualizando o valor da pilha
-                vlp[cont_vlp].pos_pilha = tampilha; // a posição da pilha da variável é o valor do tamanho da pilha
-                cont_vlp++; // atualizando o contador de variáveis locais
-                
+            if (sscanf(linha, "var vi%d", &vl.var[vl.var_qtd].ind) == 1) {
+                rsp += 4;
+                vl.var[vl.var_qtd].pos = rsp;
+                vl.var_qtd++;
+                continue;
+            }
+            if (sscanf(linha, "vet va%d size ci%d", &vl.vet[vl.vet_qtd].ind, &vl.vet[vl.vet_qtd].size) == 2) {
+                alinhar_rsp(&rsp, 8);
+                rsp += 8 * vl.vet[vl.vet_qtd].size;
+                vl.vet[vl.vet_qtd].pos = rsp;
+                vl.vet_qtd++;
                 continue;
             }        
         }
@@ -161,9 +109,12 @@ int main() {
         if (bloco_funcao) {
 
             // código do corpo da função aqui (cálculos, chamadas de outras funções, etc)
-
             if (strncmp(linha, "end", 3) == 0) { // verifica se a linha é uma finalização de função
                 
+                recuperar_vl(vl);
+
+                printf("    addq    $%d, %%rsp\n", rsp);
+
                 // código da finalização de função aqui (recuperação de v)
                 printf("\n    leave\n");
                 printf("    ret\n\n");
@@ -171,27 +122,24 @@ int main() {
                 bloco_funcao = false; // finaliza o corpo da função
                 bloco_declaracao = true; // permite o início de uma nova declaração de função
                 
-                // agora é necessário reiniciar os valores das variáveis para a próxima função
-                reinicializa_parametros(&indice_funcao, &nro_param, tipo_param, &tampilha, vlp, ind_reg, &cont_reg, &cont_vlp);
-                iniciar_registradores(r);
-
                 continue;
             }
 
             if (strncmp(linha, "call", 3) == 0) { // verifica se a linha é uma chamada de função
-                sscanf(linha, "call %s", nomefuncao); // TEM QUE FAZER
-                salvar_parametros(nro_param, &tampilha, r);
+                sscanf(linha, "call %s", nomefuncao);
+
+                // rsp -= padding;
                 
-                salvar_reg(r, &tampilha, salvos);
+                salvar_parametros(&param, &rsp); // salvar e mostrar que salvou os parâmetros da declaração da função
+                alinhar_rsp(&rsp, 16);
                 
                 printf("    call %s\n", nomefuncao);
-
-                recuperar_reg(r, &tampilha, salvos);
-
-                recuperar_parametros(nro_param, &tampilha, r);
+                
+                recuperar_parametros(&param, &rsp); // recuperar e mostrar que recuperou os parâmetros da declaração da função
+                
+                // rsp += padding;
             }
         }
-
     }
 
     return 0;
